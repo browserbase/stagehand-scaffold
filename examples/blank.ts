e2408d3d243958bd20c462b15ef3577eb410ab5c
Anirudh Kamath <github@kamath.io>
2025-03-31 01:46:01 -0700

    update 2.0

--- examples/blank.ts
+++ examples/blank.ts
@@ -1,23 +1,25 @@
-/**
- * 🤘 Welcome to Stagehand!
- *
- * TO RUN THIS PROJECT:
- * ```
- * npm install
- * npm run start
- * ```
- *
- * To edit config, see `stagehand.config.ts`
- *
- */
-import { Page, BrowserContext, Stagehand } from "@browserbasehq/stagehand";
-import { z } from "zod";
+import { Stagehand, Page, BrowserContext } from "@browserbasehq/stagehand";
+import StagehandConfig from "./stagehand.config.js";
 import chalk from "chalk";
-import dotenv from "dotenv";
+import boxen from "boxen";
+import { drawObserveOverlay, clearOverlays, actWithCache } from "./utils.js";
+import { z } from "zod";
 
-dotenv.config();
-
-export async function main({
+/**
+ * 🤘 Welcome to Stagehand! Thanks so much for trying us out!
+ *
+ * 📝 Check out our docs for more fun use cases, like building agents
+ * https://docs.stagehand.dev/
+ *
+ * 💬 If you have any feedback, reach out to us on Slack!
+ * https://stagehand.dev/slack
+ *
+ * 📚 You might also benefit from the docs for Zod, Browserbase, and Playwright:
+ * - https://zod.dev/
+ * - https://docs.browserbase.com/
+ * - https://playwright.dev/docs/intro
+ */
+async function main({
   page,
   context,
   stagehand,
@@ -26,5 +28,54 @@ export async function main({
   context: BrowserContext; // Playwright BrowserContext
   stagehand: Stagehand; // Stagehand instance
 }) {
-  // Add your code here
+  // Navigate to a URL
+  await page.goto("https://docs.stagehand.dev/reference/introduction");
+
+  /**
+   * Your code here!
+   */
 }
+
+/**
+ * This is the main function that runs when you do npm run start
+ *
+ * YOU PROBABLY DON'T NEED TO MODIFY ANYTHING BELOW THIS POINT!
+ *
+ */
+async function run() {
+  const stagehand = new Stagehand({
+    ...StagehandConfig,
+  });
+  await stagehand.init();
+
+  if (StagehandConfig.env === "BROWSERBASE" && stagehand.browserbaseSessionID) {
+    console.log(
+      boxen(
+        `View this session live in your browser: \n${chalk.blue(
+          `https://browserbase.com/sessions/${stagehand.browserbaseSessionID}`
+        )}`,
+        {
+          title: "Browserbase",
+          padding: 1,
+          margin: 3,
+        }
+      )
+    );
+  }
+
+  const page = stagehand.page;
+  const context = stagehand.context;
+  await main({
+    page,
+    context,
+    stagehand,
+  });
+  await stagehand.close();
+  console.log(
+    `\n🤘 Thanks so much for using Stagehand! Reach out to us on Slack if you have any feedback: ${chalk.blue(
+      "https://stagehand.dev/slack"
+    )}\n`
+  );
+}
+
+run();
